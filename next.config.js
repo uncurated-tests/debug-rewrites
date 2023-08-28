42bad79d3dceca96e3e8577b6fc0936d91618a78
Steven Tey <stevensteel97@gmail.com>
2023-08-28 15:26:29 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: "/ray",
+        destination:
+          "https://ray.so",
+      },
+      {
         source: "/retool",
         destination:
           "https://winfakt.retool.com/embedded/public/b8088c16-7409-4daf-989a-689bdb221371",
