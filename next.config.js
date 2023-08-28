7ca186e652fc50fa5f46d30dc698f6664ad7c201
Steven Tey <stevensteel97@gmail.com>
2023-08-28 15:21:40 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/retool",
+        destination:
+          "https://winfakt.retool.com/embedded/public/b8088c16-7409-4daf-989a-689bdb221371",
+      },
+      {
+        source: "/youtube",
+        destination:
+          "https://youtube.com/@stey",
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
