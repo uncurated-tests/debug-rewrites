169b26531c3c43e7aac0514db21fadf6bc3614b7
Steven Tey <stevensteel97@gmail.com>
2023-08-28 15:38:43 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   async rewrites() {
     return [
-      // CSS is not loaded because https://ray.so is executed via Next.js as well
+      // URL: https://debug-rewrites.vercel.app/ray
+      // Issue: CSS is not loaded because https://ray.so is executed via Next.js as well
       // adding `assetPrefix: https://ray.so` in the destination site's `next.config.js` file should fix this
       // GitHub issue ref: https://github.com/vercel/next.js/discussions/20650#discussioncomment-4644834
       {
@@ -10,14 +11,16 @@ const nextConfig = {
         destination:
           "https://ray.so",
       },
-      // Destination site throws a 404 because for some reason, the rewrite is executed on 
+      // URL: https://debug-rewrites.vercel.app/retool
+      // Issue: Destination site throws a 404 because for some reason, the rewrite is executed on 
       // https://winfakt.retool.com/retool instead of the correct destination URL
       {
         source: "/retool",
         destination:
           "https://winfakt.retool.com/embedded/public/b8088c16-7409-4daf-989a-689bdb221371",
       },
-      // The link redirects instead of rewrites
+      // URL: https://debug-rewrites.vercel.app/youtube
+      // Issue: The link redirects instead of rewrites
       {
         source: "/youtube",
         destination:
